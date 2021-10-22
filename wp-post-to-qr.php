f927528d81818e37c4339617026907a95cf4ec79
khalidwebmail <csewebmail@gmail.com>
2021-10-22 20:06:33 +0600

    show qr code for single post

--- wp-post-to-qr.php
+++ wp-post-to-qr.php
@@ -21,3 +21,87 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Update URI:        https://github.com/Khalidwebmail/wp-post-to-qr
  */
+
+use QR\Code\Wp_Code_Settings;
+use QR\Code\Wp_Generate_Code;
+
+if( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Include autoload file
+ */
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
+
+/**
+ * Class Wp_Post_QR
+ */
+final class Wp_Post_QR {
+	/**
+	 * Wp_Post_QR constructor.
+	 */
+	private function __construct() {
+		$this->wp_ptqr_define_constants();
+		add_action( 'init', [ $this, 'wp_ptqr_init_plugin' ] );
+	}
+
+	/**
+	 * Plugin version
+	 */
+	public const version = '1.0.0';
+
+	/**
+	 * Initialize singleton instance
+	 *
+	 * @return \Wp_Post_QR
+	 */
+	public static function wp_ptqr_init() {
+		static $instance  = false;
+
+		if(! $instance) {
+			$instance = new self();
+		}
+		return $instance;
+	}
+
+	/**
+	 * Define required plugins constants
+	 *
+	 * @return void
+	 */
+	public function wp_ptqr_define_constants() {
+		define( 'WP_PTQR_VERSION', self::version );
+		define( 'WP_PTQR_FILE', __FILE__ );
+		define( 'WP_PTQR_PATH', __DIR__ );
+		define( 'WP_PTQR_URL', plugins_url('', WP_PTQR_FILE ) );
+		define( 'WP_PTQR_ASSETS', WP_PTQR_URL . '/assets' );
+	}
+
+	/**
+	 * Initialize the plugin
+	 *
+	 * @return void
+	 */
+
+	public function wp_ptqr_init_plugin() {
+		new Wp_Generate_Code();
+        new Wp_Code_Settings();
+	}
+}
+
+/**
+ * Initialize the main plugin
+ *
+ * @return \Wp_Post_QR
+ */
+function wp_ptqr_boot() {
+	return Wp_Post_QR::wp_ptqr_init();
+}
+
+/**
+ * Kick of plugin
+ */
+wp_ptqr_boot();
