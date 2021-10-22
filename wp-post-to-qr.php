d424554f9751b9a2b983c045f9e0d1f586a39f59
khalidwebmail <csewebmail@gmail.com>
2021-10-22 19:32:53 +0600

    initial commit

---
+++ wp-post-to-qr.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * QR code for post
+ *
+ * @package           PluginPackage
+ * @author            Khalid Ahmed
+ * @copyright         2021 Khalid Ahmed
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       QR code for post
+ * Plugin URI:        https://github.com/Khalidwebmail/wp-post-to-qr
+ * Description:       Description of the plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.6
+ * Requires PHP:      7.0
+ * Author:            Khalid Ahmed
+ * Author URI:        https://example.com
+ * Text Domain:       qr-code-for-post
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://github.com/Khalidwebmail/wp-post-to-qr
+ */
