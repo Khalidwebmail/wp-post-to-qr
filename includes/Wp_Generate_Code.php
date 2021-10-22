183f91b69a8d3454536124b0713de5c77bed8d33
khalidwebmail <csewebmail@gmail.com>
2021-10-22 22:16:43 +0600

    correct code indent

--- includes/Wp_Generate_Code.php
+++ includes/Wp_Generate_Code.php
@@ -17,14 +17,16 @@ class Wp_Generate_Code {
      * @return $content
      */
     public function wp_ptqr_display_code( $content ) {
+        global $post;
+
         $current_post_id        = get_the_ID();
         $current_post_title     = get_the_title( $current_post_id );
         $current_post_url       = urlencode( get_the_permalink( $current_post_id ) );
+        $author_email           = get_the_author_meta( 'user_email', $post->post_author );
+        $height                 = get_option('pqrc_height') ? get_option('pqrc_height') : 150;;
+        $width                  = get_option('pqrc_width')  ? get_option('pqrc_width')  : 150;;
 
-        $height = get_option('pqrc_height') ? get_option('pqrc_height') : 150;;
-        $width  = get_option('pqrc_width')  ? get_option('pqrc_width')  : 150;;
-
-        $img_src                = sprintf( "https://api.qrserver.com/v1/create-qr-code/?size={$width}x{$height}&data=%s", $current_post_url );
+        $img_src                = sprintf( "https://api.qrserver.com/v1/create-qr-code/?size={$width}x{$height}&data=%s&email=%s", $current_post_url, $author_email );
         $content .= sprintf( "<div class='qrcode'><img src='%s' alt='%s'></div>", $img_src, $current_post_title );
         return $content;
     }
